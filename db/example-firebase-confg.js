a64fc154bb52178f627943c98abb9ed73556dc0d
Michael Maquera <mike.maquera212@gmail.com>
2022-09-19 13:21:00 -0400

    updated git ignore

---
+++ db/example-firebase-confg.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+// Example Firebase config
+/*
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+*/
+
+// Initialize Firebase App with default settings
+//export const app = initializeApp(firebaseConfig);
